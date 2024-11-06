383354a0f8d4d4389448f4b143cb417c407801ec
Hazem-Hegazy <hazemhegazy.01210011@gmail.com>
2024-11-06 15:17:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const costPerKilometer = 5; // Cost per kilometer
+const costPerKilometer = 3; // Cost per kilometer
 const defaultWeight = 5; // Default weight in kg
-const defaultWeightCost = 15; // Cost for 5 kg
+const defaultWeightCost = 5; // Cost for 5 kg
+const fragileFee = 10; // رسوم المنتجات الحساسة
+const perishableFee = 15; // رسوم المنتجات القابلة للتلف
 
 function calculateCost() {
     const distance = parseFloat(document.getElementById("distance").value);
     const weight = parseFloat(document.getElementById("weight").value) || defaultWeight; // Use default weight if not provided
-    
+    const productType = document.getElementById("productType").value;
+
     if (!distance || distance <= 0) {
         alert("Please enter a valid distance.");
         return;
     }
 
-    const totalCost = (distance * costPerKilometer) + (weight === defaultWeight ? defaultWeightCost : weight * costPerKilometer);
+    let totalCost = (distance * costPerKilometer) + (weight === defaultWeight ? defaultWeightCost : weight * costPerKilometer);
+
+    if (productType === "fragile") {
+        totalCost +=fragileFee;
+    } else if (productType === "perishable") {
+        totalCost += perishableFee
+    }
+
+    // عرض النتيجة
     document.getElementById("result").innerText = `Total Delivery Cost: ${totalCost} EGP`;
 }
